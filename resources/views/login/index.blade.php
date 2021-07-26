8e4646464397037e76387957627e3c363db1df10
Tisya Nada Fitriana <tisyanada21@gmail.com>
2021-07-26 22:53:42 +0700

    update UI welcome page and login page

--- resources/views/login/index.blade.php
+++ resources/views/login/index.blade.php
@@ -1,94 +1,138 @@
 <!DOCTYPE html>
 <html lang="en">
-
 <head>
-  <meta charset="utf-8">
-  <meta http-equiv="X-UA-Compatible" content="IE=edge">
-  <title>Login</title>
-  <link rel='icon' href="{{asset('dist/img/favicon_suzuki.ico')}}" type='image/x-icon'>
-  <!-- Tell the browser to be responsive to screen width -->
-  <meta name="viewport" content="width=device-width, minimum-scale=1, maximum-scale=1" />
 
-  <!-- v4.0.0-alpha.6 -->
-  <link rel="stylesheet" href="{{asset('dist/bootstrap/css/bootstrap.min.css')}}">
+     <title>Login</title>
+<!--
 
-  <!-- Google Font -->
-  <link href="https://fonts.googleapis.com/css?family=Poppins:300,400,500,600,700" rel="stylesheet">
+DIGITAL TREND
 
-  <!-- Theme style -->
-  <link rel="stylesheet" href="{{asset('dist/css/style.css')}}">
-  <link rel="stylesheet" href="{{asset('dist/css/font-awesome/css/font-awesome.min.css')}}">
-  <link rel="stylesheet" href="{{asset('dist/css/et-line-font/et-line-font.css')}}">
-  <link rel="stylesheet" href="{{asset('dist/css/themify-icons/themify-icons.css')}}">
+https://templatemo.com/tm-538-digital-trend
 
-  <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
-  <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
-  <!--[if lt IE 9]>
-  <script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
-  <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
-<![endif]-->
-  <header class="main-header">
-    <!-- Logo -->
-    <a href="index.html" class="logo blue-bg">
-      <!-- mini logo for sidebar mini 50x50 pixels -->
-      <span class="logo-mini"><img style="height: auto; width: 90%;" src="{{asset('dist/img/suzuki-small.png')}}" alt=""></span>
-      <!-- logo for regular state and mobile devices -->
-      <span class="logo-lg"><img style="height: auto; width: 90%;" src="{{asset('dist/img/suzuki.svg')}}" alt=""></span>
-    </a>
-    <!-- Header Navbar: style can be found in header.less -->
-    <nav class="navbar blue-bg navbar-static-top">
-      <!-- Sidebar toggle button-->
+-->
+     <meta charset="UTF-8">
+     <meta http-equiv="X-UA-Compatible" content="IE=Edge">
+     <meta name="description" content="">
+     <meta name="keywords" content="">
+     <meta name="author" content="">
+     <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
 
-    </nav>
-  </header>
+     <link rel='icon' href="{{asset('dist/img/favicon_suzuki.ico')}}" type='image/x-icon'>
+     <link rel="stylesheet" href="{{asset('dist/css2/bootstrap.min.css')}}">
+     <link rel="stylesheet" href="{{asset('dist/css2/font-awesome.min.css')}}">
+     <link rel="stylesheet" href="{{asset('dist/css2/aos.css')}}">
+     <link rel="stylesheet" href="{{asset('dist/css2/owl.carousel.min.css')}}">
+     <link rel="stylesheet" href="{{asset('dist/css2/owl.theme.default.min.css')}}">
+
+     <link rel="stylesheet" href="{{asset('dist/css/style.css')}}">
+     <link rel="stylesheet" href="{{asset('dist/css/font-awesome/css/font-awesome.min.css')}}">
+     <link rel="stylesheet" href="{{asset('dist/css/et-line-font/et-line-font.css')}}">
+     <link rel="stylesheet" href="{{asset('dist/css/themify-icons/themify-icons.css')}}">
+
+     <!-- MAIN CSS -->
+     <link rel="stylesheet" href="{{asset('dist/css2/templatemo-digital-trend.css')}}">
+
+
 </head>
-
 <body>
-  <div class="content-header sty-one">
-    <ol class="breadcrumb">
-      <li><a href="{{ url('/') }}">Sistem Penilaian Kinerja</a></li>
-      <li class="sub-bread"><i class="fa fa-angle-right"></i> Log in</li>
-    </ol>
-  </div>
-  <br><br><br>
-  <center>
-    <div class="col-lg-4 m-b-3" height="100vh">
-      <div class="card" align="center">
-        <div class="card-body">
-          <h3 class="login-box-msg">Log In</h3>
-          <form action="{{ url('login') }}" method="post">
-            {{csrf_field()}}
-            @if (Session::has('error_login'))
-            <div class="alert alert-danger">
-              {{ Session::get('error_login') }}
-            </div>
-            @endif
-            <div class="form-group has-feedback">
-              <input type="text" autocomplete="off" class="form-control sty1" name="username" placeholder="User">
-            </div>
-            <div class="form-group has-feedback">
-              <input type="password" class="form-control sty1" name="password" placeholder="Password">
-            </div>
-            <button type="submit" class="btn btn-primary">Sign In</button>
+
+     <!-- MENU BAR -->
+    <nav class="navbar navbar-expand-lg position-absolute">
+        <div class="container">
+          <a class="navbar-brand" href="{{ url('/') }}">
+              Sistem Penilaian Kinerja Karyawan
+            </a>
+
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false"
+                aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
         </div>
-        </form>
+    </nav>
+
+
+     <!-- CONTACT -->
+     <section class="contact section-padding">
+          <div class="container">
+               <div class="row">
+
+                    <div class="col-lg-6 mx-auto col-md-7 col-12 py-5 mt-5 text-center" data-aos="fade-up">
+
+                      <h1 class="mb-4">Silahkan Login untuk <strong>menilai kinerja</strong></h1>
+
+                    </div>
+
+                    <div class="col-lg-8 mx-auto col-md-10 col-12">
+                    
+                    <center>
+                      <div class="card" align="center">
+                        <div class="card-body">
+                          <h3 class="login-box-msg">Log In</h3>
+                          <form action="{{ url('login') }}" method="post">
+                          {{csrf_field()}}
+                          @if (Session::has('error_login'))
+                          <div class="alert alert-danger">
+                            {{ Session::get('error_login') }}
+                          </div>
+                          @endif
+                        <div class="form-group has-feedback">
+                          <input type="text" autocomplete="off" class="form-control sty1" name="username" placeholder="User">
+                        </div>
+                        <div class="form-group has-feedback">
+                          <input type="password" class="form-control sty1" name="password" placeholder="Password">
+                        </div>
+                        <button type="submit" class="btn btn-primary">Sign In</button>
+                      </div>
+                    </form>
       </div>
     </div>
     </div>
   </center>
-  <footer class="main-footer" style="margin-left: 0;">
-    <div class="pull-right hidden-xs">Version 1.2</div>
-    Copyright © 2021 PT. Dwi Perkasa Mobiltama Cibubur
-  </footer>
-  <!-- jQuery 3 -->
-  <script src="{{asset('dist/js/jquery.min.js')}}"></script>
+                    </div>
 
-  <!-- v4.0.0-alpha.6 -->
-  <script src="{{asset('dist/bootstrap/js/bootstrap.min.js')}}"></script>
+               </div>
+          </div>
+     </section>
 
-  <!-- template -->
-  <script src="{{asset('dist/js/niche.js')}}"></script>
-  @include('login.login_js')
+     <footer class="site-footer">
+      <div class="container">
+        <div class="row">
+
+          <div class="col-lg-5 mx-lg-auto col-md-8 col-10">
+            <h1 class="text-white" data-aos="fade-up" data-aos-delay="100">PT. Dwi Perkasa Mobiltama <strong>Cibubur.</strong></h1>
+          </div>
+
+          <div class="col-lg-3 col-md-6 col-12" data-aos="fade-up" data-aos-delay="200">
+            <h4 class="my-4">Kontak Kami</h4>
+
+            <p class="mb-1">
+              +62 21 84301921
+            </p>
+          </div>
+
+          <div class="col-lg-3 col-md-6 col-12" data-aos="fade-up" data-aos-delay="300">
+            <h4 class="my-4">Kantor</h4>
+
+            <p class="mb-1">
+              Trans Yogie, Jl. Alternatif Cibubur Jl. Raya Kranggan No.6, RT.001/RW.001, Nagrak, Kec. Jatisampurna, Kota Bks, Jawa Barat 17433, Indonesia
+            </p>
+          </div>
+            <p class="copyright-text">Copyright © 2021 PT. Dwi Perkasa Mobiltama Cibubur
+            <br>
+            <a rel="nofollow noopener">Version 1.2</a></p>
+          </div>
+        </div>
+      </div>
+    </footer>
+
+
+    <!-- SCRIPTS -->
+     <script src="{{asset('dist/js2/jquery.min.js')}}"></script>
+     <script src="{{asset('dist/js2/bootstrap.min.js')}}"></script>
+     <script src="{{asset('dist/js2/aos.js')}}"></script>
+     <script src="{{asset('dist/js2/owl.carousel.min.js')}}"></script>
+     <script src="{{asset('dist/js2/smoothscroll.js')}}"></script>>
+     <script src="{{asset('dist/js2/custom.js')}}"></script>
+
 </body>
-
 </html>
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,185 +1,147 @@
 <!DOCTYPE html>
 <html lang="en">
-
 <head>
-    <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <title>Sistem Penilaian Kinerja Karyawan </title>
-    <style>
-        /* Make the image fully responsive */
-        .carousel-inner img {
-            width: 100%;
-            height: 100%;
-        }
-    </style>
-    <link rel='icon' href="{{asset('dist/img/favicon_suzuki.ico')}}" type='image/x-icon'>
-    <!-- Tell the browser to be responsive to screen width -->
-    <meta name="viewport" content="width=device-width, minimum-scale=1, maximum-scale=1" />
 
-    <!-- v4.0.0-alpha.6 -->
-    <link rel="stylesheet" href="{{asset('dist/bootstrap/css/bootstrap.min.css')}}">
+     <title>Sistem Penilaian Kinerja Karyawan</title>
+<!--
 
-    <!-- Google Font -->
-    <link href="https://fonts.googleapis.com/css?family=Poppins:300,400,500,600,700" rel="stylesheet">
+DIGITAL TREND
 
-    <!-- Theme style -->
-    <link rel="stylesheet" href="{{asset('dist/css/style.css')}}">
-    <link rel="stylesheet" href="{{asset('dist/css/landing.css')}}">
-    <link rel="stylesheet" href="{{asset('dist/css/font-awesome/css/font-awesome.min.css')}}">
-    <link rel="stylesheet" href="{{asset('dist/css/et-line-font/et-line-font.css')}}">
-    <link rel="stylesheet" href="{{asset('dist/css/themify-icons/themify-icons.css')}}">
+https://templatemo.com/tm-538-digital-trend
 
-    <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
-    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
-    <!--[if lt IE 9]>
-  <script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
-  <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
-<![endif]-->
-    <header class="main-header">
-        <!-- Logo -->
-        <a href="index.html" class="logo blue-bg">
-            <!-- mini logo for sidebar mini 50x50 pixels -->
-            <span class="logo-mini"><img style="height: auto; width: 90%;" src="{{asset('dist/img/suzuki-small.png')}}"
-                    alt=""></span>
-            <!-- logo for regular state and mobile devices -->
-            <span class="logo-lg"><img style="height: auto; width: 90%;" src="{{asset('dist/img/suzuki.svg')}}"
-                    alt=""></span>
-        </a>
-        <!-- Header Navbar: style can be found in header.less -->
-        <nav class="navbar blue-bg navbar-static-top">
-            <br>
-            <div class="navbar-custom-menu">
-                <ul class="nav navbar-nav">
-                    <a href="{{ url('/login') }}" type="button" class="btn btn-nor btn-warning">Log In</button></a>
+-->
+     <meta charset="UTF-8">
+     <meta http-equiv="X-UA-Compatible" content="IE=Edge">
+     <meta name="description" content="">
+     <meta name="keywords" content="">
+     <meta name="author" content="">
+     <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+
+     <link rel='icon' href="{{asset('dist/img/favicon_suzuki.ico')}}" type='image/x-icon'>
+     <link rel="stylesheet" href="{{asset('dist/css2/bootstrap.min.css')}}">
+     <link rel="stylesheet" href="{{asset('dist/css2/font-awesome.min.css')}}">
+     <link rel="stylesheet" href="{{asset('dist/css2/aos.css')}}">
+     <link rel="stylesheet" href="{{asset('dist/css2/owl.carousel.min.css')}}">
+     <link rel="stylesheet" href="{{asset('dist/css2/owl.theme.default.min.css')}}">
+
+     <!-- MAIN CSS -->
+     <link rel="stylesheet" href="{{asset('dist/css2/templatemo-digital-trend.css')}}">
+
+</head>
+<body>
+
+     <!-- MENU BAR -->
+    <nav class="navbar navbar-expand-lg">
+        <div class="container">
+            <a class="navbar-brand" href="{{ url('/') }}">
+              Sistem Penilaian Kinerja Karyawan
+            </a>
+
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false"
+                aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+
+            <div class="collapse navbar-collapse" id="navbarNav">
+                <ul class="navbar-nav ml-auto">
+                    <li class="nav-item">
+                        <a href="{{ url('/login') }}" class="nav-link contact">Login</a>
+                    </li>
                 </ul>
             </div>
-        </nav>
-    </header>
-</head>
-
-<body>
-    <div id="demo" class="carousel slide" data-ride="carousel">
-
-        <!-- Indicators -->
-        <ul class="carousel-indicators">
-            <li data-target="#demo" data-slide-to="0" class="active"></li>
-            <li data-target="#demo" data-slide-to="1"></li>
-            <li data-target="#demo" data-slide-to="2"></li>
-        </ul>
-
-        <!-- The slideshow -->
-        <div class="carousel-inner">
-            <div class="carousel-item active">
-                <img src="{{asset('dist/img/logo-dpm.png')}}" alt="Los Angeles" width="1100" height="500">
-            </div>
-            <div class="carousel-item">
-                <img src="{{asset('dist/img/suzuki_cibubur.jpg')}}" alt="Chicago" width="1100" height="100">
-            </div>
-            <div class="carousel-item">
-                <img src="{{asset('dist/img/suzuki_cibubur.jpg')}}" alt="New York" width="1100" height="500">
-            </div>
         </div>
+    </nav>
 
-        <!-- Left and right controls -->
-        <a class="carousel-control-prev" href="#demo" data-slide="prev">
-            <span class="carousel-control-prev-icon"></span>
-        </a>
-        <a class="carousel-control-next" href="#demo" data-slide="next">
-            <span class="carousel-control-next-icon"></span>
-        </a>
-    </div>
-    <div class="col-lg-12 m-t-6">
-        <div class="row">
-            <div class="col-sm-6">
-                <div class="card">
-                    <div class="card-body">
-                        <h4 class="card-title">Special title treatment</h4>
-                        <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
-                        <a href="#" class="btn btn-primary">Go somewhere</a>
+
+     <!-- HERO -->
+     <section class="hero hero-bg d-flex justify-content-center align-items-center">
+               <div class="container">
+                    <div class="row">
+
+                        <div class="col-lg-6 col-md-10 col-12 d-flex flex-column justify-content-center align-items-center">
+                              <div class="hero-text">
+
+                                   <h1 class="text-white" data-aos="fade-up">Kejujuranmu menentukan karir para karyawan</h1>
+
+                                   <a href="{{ url('/login') }}" class="custom-btn btn-bg btn mt-3" data-aos="fade-up" data-aos-delay="100">Yuk, nilai kinerja karyawan!</a>
+                              </div>
+                        </div>
+
+                        <div class="col-lg-6 col-12">
+                          <div class="hero-image" data-aos="fade-up" data-aos-delay="300">
+
+                            <img src="{{asset('dist/images/working-remotely.png')}}" class="img-fluid" alt="working girl">
+                          </div>
+                        </div>
+
                     </div>
-                </div>
-            </div>
-            <div class="col-sm-6">
-                <div class="card">
-                    <div class="card-body">
-                        <h4 class="card-title">Special title treatment</h4>
-                        <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
-                        <a href="#" class="btn btn-warning">Go somewhere</a>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div>
-    <br><br>
-    <div class="col-md-6">
-        <div class="card">
-            <div class="card-body">
-                <div class="row">
-                    <div class="col-md-6">
-                        <div class="info-box">
-                            <h5 class="m-b-2 text-black">CARA MELAKUKAN PENILAIAN</h5>
-                            <div class="sl-item sl-primary">
-                                <div class="sl-content"><small class="text-muted"><i
-                                            class="fa fa-check-circle position-left"></i> Langkah 1</small>
-                                    <p>
-                                        Kepala cabang atau kepala divisi melakukan Log In sesuai dengan username dan
-                                        password yang sudah terdaftar.
-                                    </p>
-                                </div>
-                            </div>
-                            <div class="sl-item sl-success">
-                                <div class="sl-content"><small class="text-muted"><i
-                                            class="fa fa-check-circle position-left"></i> Langkah 2</small>
-                                    <p>
-                                        Setelah berhasil Log In, pilih menu "Input Penilaian".
-                                    </p>
-                                </div>
-                            </div>
-                            <div class="sl-item sl-danger">
-                                <div class="sl-content"><small class="text-muted"><i
-                                            class="fa fa-check-circle position-left"></i> Langkah 3</small>
-                                    <p>
-                                        Klik tombol "Nilai" pada nama karyawan yang ingin dinilai.
-                                    </p>
-                                </div>
-                            </div>
-                            <div class="sl-item sl-success">
-                                <div class="sl-content"><small class="text-muted"><i
-                                            class="fa fa-check-circle position-left"></i> Langkah 4</small>
-                                    <p>
-                                        Isi form penilaian sesuai dengan kinerja karyawan tersebut berdasarkan kriteria
-                                        yang sudah ditentukan.
-                                    </p>
-                                </div>
-                            </div>
-                            <div class="sl-item sl-warning">
-                                <div class="sl-content"><small class="text-muted"><i
-                                            class="fa fa-check-circle position-left"></i> Langkah 5</small>
-                                    <p>
-                                        Klik tombol "Proses" untuk melakukan proses penilaian dan klik tombol "Submit"
-                                        untuk menyimpan nilai tersebut.
-                                    </p>
-                                </div>
-                            </div>
+               </div>
+     </section>
+
+
+     <!-- ABOUT -->
+     <section class="about section-padding pb-0" id="about">
+          <div class="container">
+               <div class="row">
+
+                    <div class="col-lg-7 mx-auto col-md-10 col-12">
+                         <div class="about-info">
+
+                              <h2 class="mb-4" data-aos="fade-up">Tujuan <strong>Sistem Penilaian Kinerja</strong> Karyawan</h2>
+
+                              <p class="mb-0" data-aos="fade-up"> Penilaian kinerja karyawan berguna untuk mengetahui kinerja para karyawan terhadap tanggung jawab yang telah diberikan.
+                              <br><br>Sistem ini dirancang untuk <strong>memudahkan</strong> para penilai (kepala cabang dan kepala divisi) untu melakukan penilaian.</p>
+                         </div>
+
+                         <div class="about-image" data-aos="fade-up" data-aos-delay="200">
+
+                          <img src="{{asset('dist/images/pencapaian.png')}}" class="img-fluid" alt="office">
                         </div>
                     </div>
-                </div>
-            </div>
+
+               </div>
+          </div>
+     </section>
+
+    <footer class="site-footer">
+      <div class="container">
+        <div class="row">
+
+          <div class="col-lg-5 mx-lg-auto col-md-8 col-10">
+            <h1 class="text-white" data-aos="fade-up" data-aos-delay="100">PT. Dwi Perkasa Mobiltama <strong>Cibubur.</strong></h1>
+          </div>
+
+          <div class="col-lg-3 col-md-6 col-12" data-aos="fade-up" data-aos-delay="200">
+            <h4 class="my-4">Kontak Kami</h4>
+
+            <p class="mb-1">
+              +62 21 84301921
+            </p>
+          </div>
+
+          <div class="col-lg-3 col-md-6 col-12" data-aos="fade-up" data-aos-delay="300">
+            <h4 class="my-4">Kantor</h4>
+
+            <p class="mb-1">
+              Trans Yogie, Jl. Alternatif Cibubur Jl. Raya Kranggan No.6, RT.001/RW.001, Nagrak, Kec. Jatisampurna, Kota Bks, Jawa Barat 17433, Indonesia
+            </p>
+          </div>
+            <p class="copyright-text">Copyright © 2021 PT. Dwi Perkasa Mobiltama Cibubur
+            <br>
+            <a rel="nofollow noopener">Version 1.2</a></p>
+          </div>
         </div>
-    </div>
-    <br><br>
-    <footer class="main-footer" style="margin-left: 0;">
-        <div class="pull-right hidden-xs">Version 1.2</div>
-        Copyright © 2021 PT. Dwi Perkasa Mobiltama Cibubur
+      </div>
     </footer>
-    <!-- jQuery 3 -->
-    <script src="{{asset('dist/js/jquery.min.js')}}"></script>
 
-    <!-- v4.0.0-alpha.6 -->
-    <script src="{{asset('dist/bootstrap/js/bootstrap.min.js')}}"></script>
 
-    <!-- template -->
-    <script src="{{asset('dist/js/niche.js')}}"></script>
+     <!-- SCRIPTS -->
+     <script src="{{asset('dist/js2/jquery.min.js')}}"></script>
+     <script src="{{asset('dist/js2/bootstrap.min.js')}}"></script>
+     <script src="{{asset('dist/js2/aos.js')}}"></script>
+     <script src="{{asset('dist/js2/owl.carousel.min.js')}}"></script>
+     <script src="{{asset('dist/js2/smoothscroll.js')}}"></script>>
+     <script src="{{asset('dist/js2/custom.js')}}"></script>
+
 </body>
-
 </html>
